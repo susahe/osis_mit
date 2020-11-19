c3274996aea45c521ab7de263cd11c7905d656bb
susahe <sumudu.susahe@gmail.com>
2020-11-19 01:43:43 -0500

    Change Login Page by adding Certificate Verfication adn Student Registration in First Page

--- app/Views/login.php
+++ app/Views/login.php
@@ -37,6 +37,39 @@
 
 					</div>
 				</form>
+				<h3> Certificate Verfication </h3>
+				<hr>
+				<?php if (session()->get('sucess')): ?>
+				<div class="alert alert-success" role="alert">
+						<?= session()->get('sucess')?>
+					</div>
+				<?php endif; ?>
+				<form class="" action="/verifycertificate" method="post">
+					<div class="form-group">
+					<label for="certificateno"> Certificate Number </label>
+					<input type="text" class="form-control" name ="certificateno" id="certificateno" value="<?php set_value('certificateno');?>">
+					</div>
+
+					<?php if (isset($validation)): ?>
+					<div class="col-12">
+						<div class="alert alert-danger" role="alert">
+							<?= $validation->listErrors(); ?>
+						</div>
+					</div>
+					<?php endif; ?>
+					<div class ="row">
+						<button type="submit" class="btn btn-primary">Certificate Verify </button>
+					</div>
+
+
+
+					</div>
+				</form>
+				<div>
+						<h4>Student Registration </h4>
+						<a href="">School Student Application</a>
+						<a href="#">School Leaver Application </a>
+				</div>
 			</div>
 		</div>
 	</div>
